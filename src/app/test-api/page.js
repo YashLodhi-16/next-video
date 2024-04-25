ce911ba9b59b66ecd96c5d05405a8c2561ff48ad
YashLodhi-16 <96700260+YashLodhi-16@users.noreply.github.com>
2024-04-25 18:24:16 +0530

    environment variable change

--- src/app/test-api/page.js
+++ src/app/test-api/page.js
@@ -1,18 +1,30 @@
 import React from "react";
 import styles from "../page.module.css";
 const fetchData = async () => {
-  const url = "/api/userdata";
-  const res = await fetch(process.env.DEVELOPMENT_URL + url);
+  //   const url = "/api/userdata";
+  const url = `${process.env.NEXT_PUBLIC_DEVELOPMENT_URL ?? null}/api/userdata`;
+  const res = await fetch(url);
+  //   const res = await fetch(process.env.DEVELOPMENT_URL + url);
   return await res.json();
 };
 const TestApi = async () => {
   const data = await fetchData();
-  console.log("data");
   return (
-    <main className={styles.main}>
+    <main className={styles.main} style={{ alignItems: "start" }}>
       <h1 className={styles["heading-one"]}>
         This Page is Created To Test Apis.
       </h1>
+      {data &&
+        data.result.map((value, index) => {
+          return (
+            <div key={index}>
+              <h2 className={styles["heading-two"]}>Name: {value.name}</h2>
+              <p className={styles["common-text"]}>
+                Age: {value.age}, Class: {value.class}, School: {value.school}
+              </p>
+            </div>
+          );
+        })}
     </main>
   );
 };
